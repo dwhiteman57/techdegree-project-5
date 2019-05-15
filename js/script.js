7d7ad2f81a5fd5122f03798e3078de1ea877219e
Daniel Whiteman <daniel.whiteman1@gmail.com>
2019-05-15 14:42:30 -0500

    generateModal Function
    Added my generateModal function

--- js/script.js
+++ js/script.js
@@ -3,8 +3,10 @@
 // DECLARING GLOBAL VARIABLES
 //----------------------------------------------
 const galleryList = document.getElementById('gallery');
+const modalList = document.querySelector('.modal-container');
 const userUrl = 'https://randomuser.me/api/?results=12&nat=us';
 let profiles = [];
+let modals = [];
 
 
 //----------------------------------------------
@@ -19,7 +21,7 @@ function fetchData(url) {
 
 
 //----------------------------------------------
-// GENERATE HTML
+// GENERATE HTML FUNCTION
 //----------------------------------------------
 
 function generateHTML(data) {
@@ -38,10 +40,45 @@ function generateHTML(data) {
 				</div>
 		</div>
 		`;
-		profiles.push(profile);
+		//profiles.push(profile);
 	})
 	return(data);
 }
+
+
+//----------------------------------------------
+// MODAL FUNCTION
+//----------------------------------------------
+
+
+
+function generateModal(data) {
+
+    data.map(modal => {
+      const body = document.querySelector('body');
+      body.classList.add("modal-container");
+      body.innerHTML =
+      `<div class="modal-container">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src=${modal.picture.large} alt="profile picture">
+                <h3 id="name" class="modal-name cap">${modal.name.first} ${modal.name.last}</h3>
+                <p class="modal-text">${modal.email}</p>
+                <p class="modal-text cap">${modal.location.city}</p>
+                <hr>
+                <p class="modal-text">${modal.phone}</p>
+                <p class="modal-text cap">${modal.location.street} ${modal.location.city}, ${modal.location.state} ${modal.location.postcode}</p>
+                <p class="modal-text">Birthday: ${modal.dob.date}</p>
+            </div>
+        </div>
+      </div>
+      `;
+    })
+  	return(data);
+  }
+
+
 
 
 
@@ -51,3 +88,23 @@ function generateHTML(data) {
 
 fetchData(userUrl)
   .then(data => generateHTML(data.results))
+  .then(data => generateModal(data.results))
+
+
+
+
+
+
+
+//----------------------------------------------------------
+// EVENT LISTENERS (NOT FUNCTIONAL, JUST TESTING IDEAS)
+//----------------------------------------------------------
+
+
+// let close = document.getElementById("modal-close-btn");
+// let card = document.querySelector(".modal");
+//
+// close.onclick = function() {
+//   card.style.display = "none";
+//   modalList.hidden=true;
+// }
